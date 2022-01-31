dffa2f0f2c96ee460a19a28cb3e3835dd0ce5f43
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-31 14:45:27 -0500

    spins up nuxt app, performs clean up

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+ <!-- eslint-disable-next-line vue/multi-word-component-names -->
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultVue',
+}
+</script>
+
+<style>
+</style>
