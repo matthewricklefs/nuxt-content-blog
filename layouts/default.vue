b68aad665a04db827ab86228b3efcb3548e77554
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-31 15:08:08 -0500

    creates simple navigation with simple search input

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,28 @@
  <!-- eslint-disable-next-line vue/multi-word-component-names -->
 <template>
   <v-app>
+    <!-- Navigation -->
+    <TheNavigation />
     <v-main>
       <v-container>
-        <Nuxt />
+        <v-row>
+          <v-col cols="12" md="10">
+            <Nuxt />
+          </v-col>
+
+          <v-col cols="12" md="2">
+            <!-- Advertisements ... -->
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import TheNavigation from '~/components/TheNavigation.vue'
 export default {
-  name: 'DefaultVue',
+  name: 'DefaultLayout',
+  components: { TheNavigation },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'HomePage',
+  layout: 'DefaultLayout',
 }
 </script>
